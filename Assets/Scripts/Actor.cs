946c3366c6e8663ed75df84ca6ef0e2fb52305f8
Rutger Scholma <rutger-100@hotmail.com>
2017-06-21 21:04:07 +0200

    Movement

---
+++ Assets/Scripts/Actor.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Actor : MonoBehaviour {
+
+
+    public MovementController movementController;
+	// Use this for initialization
+	void Start () {
+        movementController = new MovementController(this);
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
