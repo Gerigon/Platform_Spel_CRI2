6d7624789f99bc3fc25eee71e99e43ac8fbf2bb9
Rutger Scholma <rutger-100@hotmail.com>
2017-06-21 20:40:33 +0200

    MovementController
    Die ekte sicke moves giwa :100: :joy: :joy: :joy: :fire: :fire: :100:

--- Assets/Scripts/MovementController.cs
+++ Assets/Scripts/MovementController.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class MovementController : MonoBehaviour {
 
+    private float speed = 0.05f;  
+
 	// Use this for initialization
 	void Start () {
 		
@@ -14,8 +16,20 @@ public class MovementController : MonoBehaviour {
 
         if (Input.GetKey(KeyCode.W))
         {
-
+            transform.Translate(new Vector3(0, 0, 1)*speed, Space.World);
         }
-		
-	}
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(new Vector3(0, 0, -1) * speed, Space.World);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(new Vector3(-1, 0, 0) * speed, Space.World);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(new Vector3(1, 0, 0) * speed, Space.World);
+        }
+
+    }
 }
