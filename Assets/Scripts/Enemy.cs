5fccdb85e5bf385a5bf81f1b4099f061fb046c7c
Rutger Scholma <rutger-100@hotmail.com>
2017-06-24 21:01:34 +0200

    Enemy Changed

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -2,15 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemy : MonoBehaviour {
+public class Enemy : Actor {
 
 	// Use this for initialization
-	void Start () {
+	protected override void Start () {
+        base.Start();
+        speed = 0.05f;
+        jumpPower = 5;
+
+    }
+
+    // Update is called once per frame
+    protected override void Update() {
+        base.Start();
 		
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    
 }
