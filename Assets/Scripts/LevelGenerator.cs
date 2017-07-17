9b47257e388e4c2e2d75224b8f9a5ff75f25ac19
Gerigon <Gerigon@users.noreply.github.com>
2017-07-17 18:50:33 +0200

    kk update

--- Assets/Scripts/LevelGenerator.cs
+++ Assets/Scripts/LevelGenerator.cs
@@ -27,22 +27,42 @@ public class LevelGenerator : MonoBehaviour {
 
     public void GenerateLevel(int biome, int numberOfRooms)
     {
-
+        Vector3 lastRoomPos;
         switch (biome)
         {
             case 0:
-                GenerateRoom(startRooms[biome]);
+                lastRoomPos = GenerateRoom(startRooms[biome],Vector3.zero,2);
 
                 for (int i = 0; i < numberOfRooms; i++)
                 {
-                    GenerateRoom(biomeList[0][i]);
+                    Debug.Log(lastRoomPos);
+                    lastRoomPos = GenerateRoom(biomeList[0][i], lastRoomPos + new Vector3(21, 0, 0),2,3);
                 }
                 break;
         }
     }
-
-	public void GenerateRoom(GameObject room)
+    public Vector3 GenerateRoom(GameObject room, Vector3 position)
     {
-        Instantiate(room, Vector3.zero, Quaternion.identity);
+        GameObject createdRoom = Instantiate(room, position, Quaternion.identity);
+        return position;
+    }
+    public Vector3 GenerateRoom(GameObject room, Vector3 position, int Door)
+    {
+        GameObject createdRoom = Instantiate(room, position, Quaternion.identity);
+        createdRoom.transform.GetChild(0).GetChild(1).GetChild(Door).gameObject.SetActive(true);
+        return position;
+    }
+    public Vector3 GenerateRoom(GameObject room, Vector3 position, int Door, int Door2)
+    {
+        GameObject createdRoom = Instantiate(room, position, Quaternion.identity);
+        createdRoom.transform.GetChild(0).GetChild(1).GetChild(Door).gameObject.SetActive(true);
+        createdRoom.transform.GetChild(0).GetChild(1).GetChild(Door2).gameObject.SetActive(true);
+        return position;
+    }
+    public Vector3 GenerateRoom(GameObject room, Vector3 position, int Door, int Door2, int Door3)
+    {
+        GameObject createdRoom = Instantiate(room, position, Quaternion.identity);
+
+        return position;
     }
 }
