2dae3fbfb2d037afa53e0c3204ae994f6dc27e02
Gerigon <Gerigon@users.noreply.github.com>
2017-07-20 21:47:01 +0200

    Deze code werkt niet :cancer:

--- Assets/Scripts/LevelGenerator.cs
+++ Assets/Scripts/LevelGenerator.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class LevelGenerator : MonoBehaviour {
 
+    public static LevelGenerator instance;
     public List<GameObject> startRooms = new List<GameObject>();
     public List<GameObject> roomList = new List<GameObject>();
     public List<List<GameObject>> biomeList = new List<List<GameObject>>();
+    public List<Room> Rooms = new List<Room>();
+    public List<DoorPositions> doorPositions = new List<DoorPositions>();
 
     public void Awake()
     {
+        instance = this;
         CreateBiomeList();
     }
-
     public void CreateBiomeList()
     {
         biomeList.Add(new List<GameObject>());
@@ -30,19 +33,52 @@ public class LevelGenerator : MonoBehaviour {
         switch (biome)
         {
             case 0:
+                Rooms.Add(CreateRoom(Vector3.zero));
                 
-                List<Room> Rooms = new List<Room>();
-                Rooms.Add(new Room(biomeList[0][0], Vector3.zero));
                 for (int i = 0; i < numberOfRooms-1; i++)
                 {
-                    Debug.Log(Rooms.Count);
-                    Debug.Log(Rooms[0]);
                     if (Rooms[Rooms.Count-1].GetDoor() == "North")
                     {
-                        Rooms.Add(new Room(biomeList[0][0], Rooms[Rooms.Count].transform.position + new Vector3(0, 0, 5),"North"));
+                        Debug.Log(Rooms.Count);
+                        Rooms.Add(CreateRoom(Rooms[Rooms.Count-1].transform.position + new Vector3(0,0,5),"North"));
                     }
                 }
                 break;
         }
+    }
+
+    public static Room CreateRoom(Vector3 _position)
+    {
+        Room _room = Instantiate(instance.biomeList[0][0], _position, Quaternion.identity).GetComponent<Room>();
+        Debug.Log("trying to createdoors");
+        _room.Initialize();
+        return _room;
+    }
+    public static Room CreateRoom(Vector3 _position, string directionFrom)
+    {
+        Room _room = Instantiate(instance.biomeList[0][0], _position, Quaternion.identity).GetComponent<Room>();
+        Debug.Log("trying to createdoors");
+        _room.Initialize(directionFrom);
+        return _room;
+    }
+
+    public void RegisterDoor(Vector3 _position, string _fromDirection)
+    {
+        if (doorPositions.Count != 0)
+        {
+            doorPositions[doorPositions.Count - 1].position = _position;
+            doorPositions[doorPositions.Count - 1].directions = _fromDirection;
+        }
+        else
+        {
+            doorPositions[0].position = _position;
+            doorPositions[0].directions = _fromDirection;
+        }
+    }
+
+    public class DoorPositions
+    {
+        public string directions;
+        public Vector3 position;
     }
 }
--- Assets/Scripts/Room.cs
+++ Assets/Scripts/Room.cs
@@ -4,106 +4,162 @@ using UnityEngine;
 
 public class Room : MonoBehaviour
 {
-    private Room roomScript;
     public List<GameObject> northDoor;
     public List<GameObject> westDoor;
     public List<GameObject> southDoor;
     public List<GameObject> eastDoor;
 
-    GameObject _room;
+    private Room roomScript;
 
-    public Room(GameObject room, Vector3 position)
-    {
-        _room = Instantiate(room, position, Quaternion.identity) as GameObject;
-        _room.AddComponent<Room>();
-    }
-    public Room(GameObject room, Vector3 position, string directionFrom)
-    {
-        _room = Instantiate(room, position, Quaternion.identity) as GameObject;
-        _room.AddComponent<Room>();
-        CreateDoors(directionFrom);
-    }
-    private void Start()
+
+
+    public void Initialize()
     {
         northDoor = new List<GameObject>();
         westDoor = new List<GameObject>();
         southDoor = new List<GameObject>();
         eastDoor = new List<GameObject>();
-        _room = transform.gameObject;
-        roomScript = _room.GetComponent<Room>();
-        for (int i = 0; i < _room.transform.GetChild(0).GetChild(1).childCount; i++)
+        roomScript = GetComponent<Room>();
+        for (int i = 0; i < transform.GetChild(0).GetChild(1).childCount; i++)
         {
-            if (_room.transform.GetChild(0).GetChild(1).GetChild(i).name.Contains("N"))
+            Debug.Log(transform.GetChild(0).GetChild(1).GetChild(i).name);
+            if (transform.GetChild(0).GetChild(1).GetChild(i).name.Contains("N"))
             {
-                roomScript.northDoor.Add(_room.transform.GetChild(0).GetChild(1).GetChild(i).gameObject);
+                northDoor.Add(transform.GetChild(0).GetChild(1).GetChild(i).gameObject);
                 continue;
             }
-            else if (_room.transform.GetChild(0).GetChild(1).GetChild(i).name.Contains("S"))
+            else if (transform.GetChild(0).GetChild(1).GetChild(i).name.Contains("S"))
             {
-                roomScript.southDoor.Add(_room.transform.GetChild(0).GetChild(1).GetChild(i).gameObject);
+                southDoor.Add(transform.GetChild(0).GetChild(1).GetChild(i).gameObject);
                 continue;
             }
-            else if (_room.transform.GetChild(0).GetChild(1).GetChild(i).name.Contains("E"))
+            else if (transform.GetChild(0).GetChild(1).GetChild(i).name.Contains("E"))
             {
-                roomScript.eastDoor.Add(_room.transform.GetChild(0).GetChild(1).GetChild(i).gameObject);
+                eastDoor.Add(transform.GetChild(0).GetChild(1).GetChild(i).gameObject);
                 continue;
             }
-            else if (_room.transform.GetChild(0).GetChild(1).GetChild(i).name.Contains("W"))
+            else if (transform.GetChild(0).GetChild(1).GetChild(i).name.Contains("W"))
             {
-                roomScript.westDoor.Add(_room.transform.GetChild(0).GetChild(1).GetChild(i).gameObject);
+                westDoor.Add(transform.GetChild(0).GetChild(1).GetChild(i).gameObject);
                 continue;
             }
         }
         CreateDoors();
     }
+    public void Initialize(string directionFrom)
+    {
+        northDoor = new List<GameObject>();
+        westDoor = new List<GameObject>();
+        southDoor = new List<GameObject>();
+        eastDoor = new List<GameObject>();
+        roomScript = GetComponent<Room>();
+        for (int i = 0; i < transform.GetChild(0).GetChild(1).childCount; i++)
+        {
+            Debug.Log(transform.GetChild(0).GetChild(1).GetChild(i).name);
+            if (transform.GetChild(0).GetChild(1).GetChild(i).name.Contains("N"))
+            {
+                northDoor.Add(transform.GetChild(0).GetChild(1).GetChild(i).gameObject);
+                continue;
+            }
+            else if (transform.GetChild(0).GetChild(1).GetChild(i).name.Contains("S"))
+            {
+                southDoor.Add(transform.GetChild(0).GetChild(1).GetChild(i).gameObject);
+                continue;
+            }
+            else if (transform.GetChild(0).GetChild(1).GetChild(i).name.Contains("E"))
+            {
+                eastDoor.Add(transform.GetChild(0).GetChild(1).GetChild(i).gameObject);
+                continue;
+            }
+            else if (transform.GetChild(0).GetChild(1).GetChild(i).name.Contains("W"))
+            {
+                westDoor.Add(transform.GetChild(0).GetChild(1).GetChild(i).gameObject);
+                continue;
+            }
+        }
+        CreateDoors(directionFrom);
+    }
 
-    private void CreateDoors()
+    public void CreateDoors()
     {
         int[] randomDoors = new int[2];
         randomDoors[0] = Random.Range(0, 5);
         randomDoors[1] = Random.Range(0, 5);
-        while (randomDoors[0] == randomDoors[1])
+        while (transform.GetChild(0).GetChild(1).GetChild(randomDoors[0]).gameObject.activeSelf || randomDoors[0] == randomDoors[1])
+        {
+            randomDoors[0] = Random.Range(0, 5);
+        }
+        while (transform.GetChild(0).GetChild(1).GetChild(randomDoors[1]).gameObject.activeSelf || randomDoors[0] == randomDoors[1])
+        {
             randomDoors[1] = Random.Range(0, 5);
+        }
+
         if (Random.Range(0, 9) > 4)
         {
             transform.GetChild(0).GetChild(1).GetChild(randomDoors[0]).gameObject.SetActive(true);
+            
             transform.GetChild(0).GetChild(1).GetChild(randomDoors[1]).gameObject.SetActive(true);
         }
         else
         {
             transform.GetChild(0).GetChild(1).GetChild(randomDoors[0]).gameObject.SetActive(true);
         }
+
+        Debug.Log("Deuren zijn geactiveerd");
     }
-    private void CreateDoors(string directionFrom)
+    public void CreateDoors(string directionFrom)
     {
+        Vector3 doorPosition = Vector3.zero;
+        int randomNum;
         switch (directionFrom)
         {
             case "North":
-                southDoor[Random.Range(0, southDoor.Count - 1)].SetActive(true);
+                randomNum = Random.Range(0, southDoor.Count - 1);
+                southDoor[randomNum].SetActive(true);
+                break;
+            case "West":
+                randomNum = Random.Range(0, southDoor.Count - 1);
+                southDoor[randomNum].SetActive(true);
+                break;
+            case "East":
+                randomNum = Random.Range(0, southDoor.Count - 1);
+                southDoor[randomNum].SetActive(true);
+                break;
+            case "South":
+                randomNum = Random.Range(0, southDoor.Count - 1);
+                southDoor[randomNum].SetActive(true);
                 break;
         }
+        CreateDoors();
     }
     public string GetDoor()
     {
-        Debug.Log(transform);
-        return "Hallo";
+        Debug.Log("deuren worden gecontroleerd");
         for (int i = 0; i < transform.GetChild(0).GetChild(1).childCount; i++)
         {
-            if (northDoor.Contains(transform.GetChild(0).GetChild(1).GetChild(i).gameObject))
+            Debug.Log("is active: " + transform.GetChild(0).GetChild(1).GetChild(i).gameObject.activeSelf);
+            if (transform.GetChild(0).GetChild(1).GetChild(i).gameObject.activeSelf)
             {
-                return "North";
-            }
-            else if (southDoor.Contains(transform.GetChild(0).GetChild(1).GetChild(i).gameObject))
-            {
-                return "South";
-            }
-            else if (westDoor.Contains(transform.GetChild(0).GetChild(1).GetChild(i).gameObject))
-            {
-                return "West";
-            }
-            else if (eastDoor.Contains(transform.GetChild(0).GetChild(1).GetChild(i).gameObject))
-            {
-                return "East";
+                if (northDoor.Contains(transform.GetChild(0).GetChild(1).GetChild(i).gameObject))
+                {
+                    LevelGenerator.instance.RegisterDoor(transform.GetChild(0).GetChild(1).GetChild(i).transform.position, "North");
+                    return "North";
+                }
+                else if (southDoor.Contains(transform.GetChild(0).GetChild(1).GetChild(i).gameObject))
+                {
+                    LevelGenerator.instance.RegisterDoor(transform.GetChild(0).GetChild(1).GetChild(i).transform.position, "South");
+                    return "South";
+                }
+                else if (westDoor.Contains(transform.GetChild(0).GetChild(1).GetChild(i).gameObject))
+                {
+                    LevelGenerator.instance.RegisterDoor(transform.GetChild(0).GetChild(1).GetChild(i).transform.position, "West");
+                    return "West";
+                }
+                else if (eastDoor.Contains(transform.GetChild(0).GetChild(1).GetChild(i).gameObject))
+                {
+                    LevelGenerator.instance.RegisterDoor(transform.GetChild(0).GetChild(1).GetChild(i).transform.position, "East");
+                    return "East";
+                }
             }
         }
         return null;
