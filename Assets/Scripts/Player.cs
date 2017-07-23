dd11eae0d5f0493890cf162a4473d33a4ec7af16
Rutger Scholma <rutger-100@hotmail.com>
2017-07-23 18:11:49 +0200

    Animation update

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -23,42 +23,6 @@ public class Player : Actor
     protected override void Update()
     {
         base.Update();
-<<<<<<< HEAD
-        if (Input.GetKey(KeyCode.W))
-        {
-            movementController.Move(0, speed);
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            movementController.Move(2, speed);
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            movementController.Move(3, speed);
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            movementController.Move(1, speed);
-        }
-        if (Input.GetKey(KeyCode.Space))
-        {
-            movementController.Jump();
-        }
-        if (Input.GetMouseButtonDown(0))
-        {
-            animationController.SetAnimationVar("Attack");
-            //combatController.PerformAttack();
-        }
-        RaycastHit[] hit = Physics.RaycastAll(mouseRay, Mathf.Infinity, 1 << 10);
-        mouseRay = Camera.main.ScreenPointToRay(Input.mousePosition);
-        for (int i = 0; i < hit.Length; i++)
-        {
-            Debug.Log(hit[i].transform.name);
-            mousePosition = hit[i].point;
-        }
-        movementController.Rotate(mousePosition);
-=======
-        
->>>>>>> bc49db4548c53f2770d7fab690348a8b9115a2b2
+
     }
 }
