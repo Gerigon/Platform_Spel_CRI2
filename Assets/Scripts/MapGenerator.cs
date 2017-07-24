01a1dad6000e9de65a9bb7f734144863900ca185
CptFrameLag <itschris_1993@hotmail.com>
2017-07-24 14:45:41 +0200

    MapGenerator

--- Assets/Scripts/MapGenerator.cs
+++ Assets/Scripts/MapGenerator.cs
@@ -20,14 +20,14 @@ public class MapGenerator : MonoBehaviour
 				void Start()
 				{
 								GenerateMap();
-								InstantiateLevel();
+								//InstantiateLevel();
 				}
 
 				void Update()
 				{
 								if (Input.GetMouseButtonDown(0))
 								{
-											//	GenerateMap();
+											GenerateMap();
 												
 								}
 				}
@@ -37,7 +37,7 @@ public class MapGenerator : MonoBehaviour
 								map = new int[width, height];
 								RandomFillMap();
 
-								for (int i = 0; i < 2; i++)
+								for (int i = 0; i < 1; i++)
 								{
 												SmoothMap();
 								}
@@ -127,20 +127,20 @@ public class MapGenerator : MonoBehaviour
 								}
 				}
 
-				//void OnDrawGizmos()
-				//{
-				//				if (map != null)
-				//				{
-				//								for (int x = 0; x < width; x++)
-				//								{
-				//												for (int y = 0; y < height; y++)
-				//												{
-				//																Gizmos.color = (map[x, y] == 1) ? Color.black : Color.white;
-				//																Vector3 pos = new Vector3(-width / 2 + x + .5f, 0, -height / 2 + y + .5f);
-				//																Gizmos.DrawCube(pos, new Vector3(3,3,3));
-				//												}
-				//								}
-				//				}
-				//}
+				void OnDrawGizmos()
+				{
+								if (map != null)
+								{
+												for (int x = 0; x < width; x++)
+												{
+																for (int y = 0; y < height; y++)
+																{
+																				Gizmos.color = (map[x, y] == 1) ? Color.black : Color.white;
+																				Vector3 pos = new Vector3(-width / 2 + x + .5f, 0, -height / 2 + y + .5f);
+																				Gizmos.DrawCube(pos, new Vector3(3, 3, 3));
+																}
+												}
+								}
+				}
 
 }
