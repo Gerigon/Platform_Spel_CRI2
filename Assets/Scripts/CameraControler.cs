1b0c1cd6d186146a4458054d02567a9700708e76
Rutger Scholma <rutger-100@hotmail.com>
2017-09-14 14:50:55 +0200

    Formating

--- Assets/Scripts/CameraControler.cs
+++ Assets/Scripts/CameraControler.cs
@@ -2,17 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraControler : MonoBehaviour {
+public class CameraControler : MonoBehaviour
+{
 
-				public Transform player;
-				public Vector3 offset;
-				// Use this for initialization
-				void Start () {
-								
-				}
-	
-				// Update is called once per frame
-				void Update () {
-								transform.position = new Vector3(player.position.x + offset.x, player.position.y + offset.y, offset.z);
-				}
+    public Transform player;
+    public Vector3 offset;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(player.position.x + offset.x, player.position.y + offset.y, offset.z);
+    }
 }
